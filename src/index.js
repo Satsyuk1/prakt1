24956c193e0df24d737e2c5c6cb7058c65c538b0
Satsyuk1 <95620934+Satsyuk1@users.noreply.github.com>
2023-01-14 10:54:03 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import { createApp } from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+
+
+const app = createApp({
+    data() {
+      return {
+        students: [],
+        search: "",
+        student: { name: "", zdav: false, group: "" },
+  
+      };
+    },
+    mounted: function () {
+      axios.get("http://34.82.81.113:3000/students").then((response) => {
+        this.students = response.data;
+      });
+      
+    },
+    methods: {
+      deleteStudent(studId) {
+        const index = this.students.findIndex((elem) => elem.id === studId);
+        this.students.splice(index, 1);
+      },
+      addStudent() {
+        this.student.id = this.students.length + 1;
+        this.students.push({ ...this.student });
+      },
+      
+    },
+  });
+  app.use(VueAxios, axios);
+  app.mount("#app");
+  
